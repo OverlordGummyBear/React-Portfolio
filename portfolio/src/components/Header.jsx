64983584dc4128eeb111c9c582e6fb1cc1f4d7b5
DANIELSTATIONÆR\danie <danielwlaursen@gmail.com>
2026-07-30 09:00:12 +0200

    Fix error with import

---
+++ portfolio/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import WebDevelopmentPic from "../assets/icons/web-development.png"
+
+function Header(){
+    return (
+            <div className="title-container">
+                <div className="title-text">
+                        <h1 style={{textIndent: "5px"}}> Hello, <br/> I'm Daniel <br/> </h1>
+                        <h2 className="h2developer" style={{textIndent: "5px"}}> A Software Developer</h2>
+                </div>
+
+                <div>
+                    <div>
+                        <img src={WebDevelopmentPic} alt="web development icon" class="web-development-icon" />
+                    </div>
+                </div>
+            </div> 
+            );
+}
+
+export default Header;
